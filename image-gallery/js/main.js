d902ba167bfc8be496f60ceb44dd17a20a8c3c90
Julia Grigoreva <julia_panina@mail.ru>
2024-09-26 22:11:03 +0300

    feat: focus on input

--- image-gallery/js/main.js
+++ image-gallery/js/main.js
@@ -2,8 +2,9 @@ const apiKey = "9Ve6CJPoODdCDtN8iXBM6uePfH1EAB64R7i0aRVhkgc";
 
 const photosWrapper = document.querySelector(".gallery-wrapper");
 const loadMore = document.querySelector(".btn-more");
+const searchInput = document.querySelector(".search__input");
 
-// const searchForm = document.querySelector(".search__input");
+window.onload = () => document.querySelector(".search__input").focus();
 
 
 // let keyword = "";
@@ -21,8 +22,11 @@ const loadMore = document.querySelector(".btn-more");
 
 const perPage = 9;
 let currentPage = 1;
+let searchTerm = null;
 
 const getPhotos = (apiURL) => {
+    loadMore.innerText = "Loading...";
+    loadMore.classList.add("disabled");
     fetch(apiURL, {
         headers: { Authorization: `Client-ID ${apiKey}` }
     })
@@ -30,6 +34,8 @@ const getPhotos = (apiURL) => {
     .then(data => {
         console.log(data);
         generateHTML(data);
+        loadMore.innerText = "Load more";
+        loadMore.classList.remove("disabled");
     })
 }
 
@@ -47,6 +53,16 @@ const loadMorePhotos = () => {
     getPhotos(apiURL);
 }
 
+const loadSearchPhotos = (e) => {
+    if(e.key === "Enter") {
+        currentPage = 1;
+        searchTerm = e.target.value;
+        photosWrapper.innerHTML = "";
+        getPhotos(`https://api.unsplash.com/search/photos?page=${currentPage}&per_page=${perPage}&query=${searchTerm}`);
+    }
+}
+
 getPhotos(`https://api.unsplash.com/photos?page=${currentPage}&per_page=${perPage}`);
 
 loadMore.addEventListener("click", loadMorePhotos);
+searchInput.addEventListener("keyup", loadSearchPhotos);
